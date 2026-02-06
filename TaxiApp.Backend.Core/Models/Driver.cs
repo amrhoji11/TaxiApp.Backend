a1df664b9db10d8ac86e3328917b82d2a1082ab7
Amr <amrhoji77@gmail.com>
2026-02-07 00:24:25 +0200

    Initial commit

---
+++ TaxiApp.Backend.Core/Models/Driver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApp.Backend.Core.Models
+{
+    public enum DriverStatus
+    {
+        available = 0,
+        busy = 1,
+        offline = 2
+    }
+    public class Driver
+    {
+        [Key]
+        public string UserId { get; set; }
+
+        public string? Address { get; set; }
+        public string? ProfilePhotoUrl { get; set; }
+
+        public DriverStatus Status { get; set; }
+
+        public decimal? LastLat { get; set; }
+        public decimal? LastLng { get; set; }
+
+        public DateTime? LastSeenAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        // Navigation
+        public ApplicationUser User { get; set; }
+
+        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public ICollection<Trip> Trips { get; set; }= new List<Trip>();
+        public ICollection<DriverLocation> Locations { get; set; } = new List<DriverLocation>();
+
+    }
+}
